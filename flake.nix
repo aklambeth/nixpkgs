a599889a399484508abb5f1c4f887330b288ba58
aklambeth <2073680+aklambeth@users.noreply.github.com>
2025-01-29 01:38:51 +0000

    Add derivations to the packages flake

--- flake.nix
+++ flake.nix
@@ -1,26 +1,23 @@
 {
-  description = "A minimal development environment";
+  description = "My custom Nix packages";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        myPackage = pkgs.callPackage ./tools/crawl4ai { };
-      in
-      {
-
-        packages.default = myPackage;
-        devShells.default = pkgs.mkShell {
-          buildInputs = [ myPackage ];
-
-          shellHook = ''
-          '';
-        };
-        
-      });
+  outputs = { self, nixpkgs }: 
+    let
+      # Supported systems
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      # Helper to provide system-specific attributes
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in {
+      packages = forAllSystems (system: 
+        let 
+          pkgs = nixpkgs.legacyPackages.${system};
+        in {
+          crawl4ai = pkgs.callPackage ./tools/crawl4ai {};
+          catppuccin_mocha-zsh-syntax-highlighting = pkgs.callPackage ./themes/catppuccin/zsh-syntax-highlighting.nix {};
+        });
+    };
 }
